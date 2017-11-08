734480a26a2c34f3348c375ef562e6640e52b3b5
Dhrubajit <dhrubajit1992@gmail.com>
2017-11-09 01:09:45 +0800

    add loader icon in Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {
 	View,
 	Text,
+  Modal,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
@@ -10,6 +11,8 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../actionCreators';
 
+// native-base
+import { Spinner } from 'native-base';
 
 // facebook oauth
 import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
@@ -22,70 +25,115 @@ if (!firebase.apps.length) {
 }
 const db = firebase.database();
 
-
-// console.log(24, config);
-
-
 class LoginPage extends Component {
 	constructor(props){
 		super(props);
-		console.log(31, props);
+    this.state = {
+      loading: false,
+    };
+    this.afterLogin = this.afterLogin.bind(this);
 	}
+
+  afterLogin(token){
+    const credential = firebase.auth.FacebookAuthProvider.credential(token);
+    firebase
+      .auth()
+      .signInWithCredential(credential)
+      .then(result => {
+        console.log('result ', result)
+        const uid = result.uid;
+        db.ref(`users/${uid}`).once('value', snapshot => {
+          const val = snapshot.val();
+          if(!!val) {
+            const user = {
+              firstName: val.firstName,
+              lastName: val.lastName,
+              email: val.email,
+              qrCode: val.qrCode,
+              photo_url: val.photo_url,
+              companyName: val.companyName,
+              companyPosition: val.companyPosition,
+              uid: val.uid,
+            }
+            this.props.actions.updateUser(user);
+            this.setState({
+              loading: false,
+            });
+            Actions.home();
+          } else {
+            this.setState({
+              loading: false,
+            });
+            Actions.profile({fbProfile: e.profile, firebase_uid: uid});
+          }
+        })
+      })
+      .catch((error) => {
+        alert('Account disabled')
+        this.setState({
+          loading: false,
+        });
+      });
+  }
 
 	render(){
 		const actions = this.props.actions;
-		return <FBLogin
-			style={{
-				textAlign: 'center',
-				flex: 1,
-				justifyContent: 'center',
-				padding: 20,
-				marginTop: 300
-			}}
-			ref={(fbLogin) => { this.fbLogin = fbLogin }}
-			loginBehavior={FBLoginManager.LoginBehaviors.Native}
-			permissions={["email"]}
-			onLogin={function(e){
-				console.log('login ', e);
-				const credential = firebase.auth.FacebookAuthProvider.credential(e.credentials.token);
-				firebase
-					.auth()
-					.signInWithCredential(credential)
-					.then(result => {
-						console.log('result ', result)
-						const uid = result.uid;
-						db.ref(`users/${uid}`).once('value', snapshot => {
-							const val = snapshot.val();
-							if(!!val) {
-								console.log(53, val);
-								const user = {
-									firstName: val.firstName,
-									lastName: val.lastName,
-									email: val.email,
-									qrCode: val.qrCode,
-									photo_url: val.photo_url,
-									companyName: val.companyName,
-									companyPosition: val.companyPosition,
-									uid: val.uid,
-								}
-								console.log(61, this.props);
-								actions.updateUser(user);
-								Actions.home();
-							} else {
-								Actions.profile({fbProfile: e.profile, firebase_uid: uid});
-							}
-						})
-					})
-					.catch((error) => alert('Account disabled'));
-				console.log('login end');
-			}}
-			onLoginFound={function(e){console.log('loginfound ', e)}}
-			onLoginNotFound={function(e){console.log('loginnotfound ', e)}}
-			onLogout={function(e){console.log('logout ', e)}}
-			onCancel={function(e){console.log('cancel ', e)}}
-			onPermissionsMissing={function(e){console.log('permissionmissing ', e)}}
-		/>
-	}
+    const afterLogin = this.afterLogin;
+    const _this = this;
+		return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+        {this.state.loading ?
+          <Modal
+            onRequestClose={() => console.log('modal closed')}
+            visible={true}
+            transparent={true}
+            animationType='fade'
+            >
+              <View style={{
+                flex: 1,
+                backgroundColor: 'rgba(238,238,238,0.4)',
+                justifyContent: 'center',
+              }}>
+                <Spinner color='blue' />
+              </View>
+          </Modal>
+          : null
+        }
+        <FBLogin
+            style={{
+              textAlign: 'center',
+              flex: 1,
+              justifyContent: 'center',
+              padding: 20,
+            }}
+            ref={(fbLogin) => { this.fbLogin = fbLogin }}
+            loginBehavior={FBLoginManager.LoginBehaviors.Native}
+            permissions={["email"]}
+            onLogin={function(e){
+              console.log('login ', e);
+              _this.setState({
+                loading: true,
+              });
+              afterLogin(e.credentials.token);
+            }}
+            onLoginFound={function(e){
+              console.log('loginfound ', e);
+              _this.setState({
+                loading: true,
+              });
+              afterLogin(e.credentials.token);
+            }}
+            onLoginNotFound={function(e){console.log('loginnotfound ', e)}}
+            onLogout={function(e){console.log('logout ', e)}}
+            onCancel={function(e){console.log('cancel ', e)}}
+            onPermissionsMissing={function(e){console.log('permissionmissing ', e)}}
+          />
+      </View>
+    );
+  }
 }
 
 export default connect((state) => ({
