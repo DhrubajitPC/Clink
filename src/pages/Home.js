b40d02a8c45a9078ca178f18aae7e2884ba665ff
Dhrubajit <dhrubajit1992@gmail.com>
2017-11-09 17:53:07 +0800

    remove unnecessary styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -305,54 +305,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     position: 'relative'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 	preview: {
-	flex: 1,
-	justifyContent: 'flex-end',
-	alignItems: 'center'
+  	flex: 1,
+  	justifyContent: 'flex-end',
+  	alignItems: 'center'
 	},
-	overlay: {
-		position: 'absolute',
-		padding: 16,
-		right: 0,
-		left: 0,
-		alignItems: 'center'
-	},
-	topOverlay: {
-		top: 0,
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center'
-	},
-	bottomOverlay: {
-		bottom: 0,
-		backgroundColor: 'rgba(0,0,0,0.4)',
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	enterBarcodeManualButton: {
-		padding: 15,
-		backgroundColor: 'white',
-		borderRadius: 40
-	},
-	scanScreenMessage: {
-		fontSize: 14,
-		color: 'white',
-		textAlign: 'center',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
 });
 
 export default connect((state) => ({
