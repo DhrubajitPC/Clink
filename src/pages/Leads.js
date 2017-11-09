bdb5af02f5716ce4d53ad6d7bbff76983b43ab1a
Dhrubajit <dhrubajit1992@gmail.com>
2017-11-09 16:20:01 +0800

    Add leads page

---
+++ src/pages/Leads.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Body,
+  Text,
+  Left,
+  Right,
+} from 'native-base';
+
+// Redux
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actionCreators';
+
+class LeadsPage extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  render() {
+    var items = Object.values(this.props.leads);
+    return (
+      <Container>
+        <Content>
+          <List dataArray={items}
+            renderRow={(item) =>
+              <ListItem avatar style={{ marginLeft: 0 }}>
+                <Left>
+                  <Thumbnail source={{ uri: item.photo_url }} />
+                </Left>
+                <Body>
+                  <Text>{this.capitalizeFirstLetter(item.firstName)} {this.capitalizeFirstLetter(item.lastName)}</Text>
+                  <Text note>{item.companyPosition}, {item.companyName}</Text>
+                  <Text note/>
+                </Body>
+                <Right justifyContent={'space-around'}>
+                  <Text style={{
+                    textAlign: 'center',
+                    fontSize: 12,
+                    color: '#00CE9F'
+                  }} onPress={() => console.log('tried to view')}>View</Text>
+                </Right>
+              </ListItem>
+            }>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+export default connect((state) => ({
+    'user': state.user,
+    'leads': state.leads,
+}), (dispatch) => ({
+    actions: bindActionCreators(actions, dispatch)
+})
+)(LeadsPage);
