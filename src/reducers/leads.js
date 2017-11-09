8a6c07897069d4ae6062984a68f195054b079644
Dhrubajit <dhrubajit1992@gmail.com>
2017-11-09 13:04:47 +0800

    bugfix leads reducer

--- src/reducers/leads.js
+++ src/reducers/leads.js
@@ -6,17 +6,16 @@ import {
 const initialState = {};
 
 export default function reducer (state = initialState, action = {}){
+  let user = {};
   switch (action.type) {
     case ADD_USER_TO_LEADS:
-      const user = action.payload;
-      return {
-        ...state,
-        user,
-      };
+      user = action.payload.user;
+      const newState = Object.assign({}, state, user);
+      return newState;
 
     case REMOVE_USER_FROM_LEADS:
       const leads = Object.assign({}, state);
-      const user = actions.payload;
+      user = actions.payload.user;
       delete leads[user.uid];
       return leads;
 
